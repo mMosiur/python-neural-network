b7670a407637c6a4f068c95cbbe24dd6a1955c97
Mateusz Piotr Moruś <mpmorus@gmail.com>
2019-03-01 22:59:13 +0100

    Implemented vectorized backpropagation

--- python-neural-network/main.py
+++ python-neural-network/main.py
@@ -11,19 +11,10 @@ TRAIN_DATA = import_data("resources\\train-images.idx3-ubyte",
 TEST_DATA = import_data("resources\\t10k-images.idx3-ubyte",
                         "resources\\t10k-labels.idx1-ubyte")
 
-NETWORK = NeuralNetwork([28*28,16,16,10])
+data = TRAIN_DATA[:1000]
 
-def test(data, nof_items=None):
-    print("Testing the network...")
-    result = NETWORK.test(data[:nof_items])
-    print("Network got {0} questions right out of {1} - {2:5.2f}% correct.\n".format(result[1], result[0], result[1] / result[0] * 100))
-def train(data, nof_items=None):
-    print("Training the network...")
-    NETWORK.train(data[:nof_items])
-    print("Finished.\n")
+NETWORK = NeuralNetwork([784,30,10])
 
-test(TEST_DATA)
-train(TRAIN_DATA)
-test(TEST_DATA)
+NETWORK.train(data, epochs=200, batch_size=10, lr=2, testing_set=data)
 
 print("End of program.\n")
--- python-neural-network/neuralnetwork.py
+++ python-neural-network/neuralnetwork.py
@@ -4,51 +4,89 @@ Module designed to construct a neural network.
 
 import numpy as np
 
+def sigmoid(x, derivative=False):
+    result = 1. / (1. + np.exp(-x))
+    if(derivative): return result * (1. - result)
+    return result
+
 def relu(x, derivative=False):
-    x[x < 0] = 0
-    if(derivative): x[x > 0] = 1
-    return x
+    if(derivative): return 1 * (x > 0)
+    return x * (x > 0)
+
+act_fun = sigmoid
 
 class Layer:
     def __init__(self, nof_neurons, nof_neurons_prev):
-        self.activations = np.zeros(nof_neurons)
+        self.activations = np.zeros([nof_neurons, 1])
         if(nof_neurons_prev > 0):
-            self.biases = np.zeros(nof_neurons)
-            self.weights = np.random.randn(nof_neurons, nof_neurons_prev) * np.sqrt(2 / nof_neurons_prev)
+            self.biases = np.random.randn(nof_neurons, 1)
+            self.weights = np.random.randn(nof_neurons, nof_neurons_prev)
 
 class NeuralNetwork:
     def __init__(self, structure):
-        l = len(structure)
-        self.layers = np.empty(l, Layer)
+        self.layers = np.empty(len(structure), Layer)
         nof_neurons = 0
-        for i in range(l):
+        for i in range(len(structure)):
             nof_neurons_prev = nof_neurons
             nof_neurons = structure[i]
             self.layers[i] = Layer(nof_neurons, nof_neurons_prev)
 
-    def think(self, input_data):
-        input_data = np.array(input_data).astype(float).ravel()
-        self.layers[0].activations = input_data
-        for i in range(1, len(self.layers)):
-            prev_a = self.layers[i - 1].activations
-            w = self.layers[i].weights
-            b = self.layers[i].biases
+    def think(self, input_data, backprop=False):
+        a = np.array(input_data).astype(float).reshape(-1,1)
+        for layer in self.layers[1:]:
+            prev_a = a
+            w = layer.weights
+            b = layer.biases
             z = w.dot(prev_a) + b
-            a = relu(z)
-            self.layers[i].activations = a
-        return self.layers[-1].activations
+            a = act_fun(z)
+            layer.activations = a
+            if(backprop): layer.weighted_sums = z
+        return a
 
-    def test(self, data):
-        result = [0,0] # [all_answers, right_answers]
-        for pair in data:
+    def test(self, testing_set):
+        np.random.shuffle(testing_set)
+        correct = 0
+        for pair in testing_set:
             guess = self.think(pair.image).argmax()
-            if(guess == pair.label): result[1] += 1
-            result[0] += 1
-        return result
+            if(guess == pair.label): correct += 1
+        return correct
 
-    def train(self, training_set):
-        nof_batches = int(np.ceil(len(training_set) / 100))
-        print("There will be {} batches of 100.".format(nof_batches))
-        for i in range(nof_batches):
-            #training on a single batch
-            pass
+    def gradient_step(self, batch, lr):
+        for layer in self.layers[1:]: # Initializing jacobian matrices for each layer
+                layer.gradient_b = np.zeros_like(layer.biases)
+                layer.gradient_w = np.zeros_like(layer.weights)
+        for pair in batch:
+            y = np.zeros([10, 1])
+            y[pair.label] = 1
+            a = self.think(pair.image, backprop=True)
+            delta = 2 * (a - y) * act_fun(self.layers[-1].weighted_sums, derivative=True)
+            self.layers[-1].gradient_b = delta
+            self.layers[-1].gradient_w = delta.dot(self.layers[-2].activations.transpose())
+            for i in range(len(self.layers) - 2, 0, -1):
+                delta = self.layers[i + 1].weights.transpose().dot(delta) * act_fun(self.layers[i].weighted_sums, derivative=True)
+                self.layers[i].gradient_b += delta
+                self.layers[i].gradient_w += delta.dot(self.layers[i - 1].activations.transpose())
+        for layer in self.layers[1:]: # Applying gradient step
+            layer.biases -= layer.gradient_b / len(batch) * lr
+            layer.weights -= layer.gradient_w / len(batch) * lr
+
+    def train(self, training_set, epochs=1, batch_size=None, lr=1.0, testing_set=None):
+        print("Training the network...")
+        for e in range(epochs):
+            np.random.shuffle(training_set)
+            if(batch_size == None): self.gradient_step(training_set, lr)
+            else:
+                for i in range(0, len(training_set), batch_size):
+                    self.gradient_step(training_set[i:i + batch_size], lr)
+            print("Epoch {} finished.".format(e + 1), end="")
+            if testing_set:
+                result = (self.test(testing_set), len(testing_set))
+                print(" Network got {} of {} right - {:5.2f}% accuracy.".format(result[0], result[1], result[0] / result[1] * 100))
+            else:
+                print("\n")
+        for layer in self.layers[1:]: # Deleting jacobian matrices of each layer
+            del layer.gradient_b
+            del layer.gradient_w
+            del layer.weighted_sums
+        print("Learning finished.")
+
